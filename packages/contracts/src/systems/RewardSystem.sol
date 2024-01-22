8a964490b1b12766e293b7075fb038eee7d2a003
PWR <pwrpntr@gmail.com>
2024-01-22 14:04:13 +0100

    fix: toke transfer

--- packages/contracts/src/systems/RewardSystem.sol
+++ packages/contracts/src/systems/RewardSystem.sol
@@ -1,35 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 import { System } from "@latticexyz/world/src/System.sol";
-import { IBaseWorld } from "@latticexyz/world/src/codegen/interfaces/IBaseWorld.sol";
-import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
-
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-import { ResourceId } from "@latticexyz/store/src/ResourceId.sol";
-import { Puppet } from "@latticexyz/world-modules/src/modules/puppet/Puppet.sol";
-
 import { GameConfig } from "../codegen/index.sol";
-import { LibUtils } from "../libraries/Libraries.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract RewardSystem is System {
   function reward() public {
-    address token = GameConfig.getTokenAddress();
-
-    address worldAddress = StoreSwitch.getStoreAddress();
-
-    ResourceId systemId = Puppet(token).systemId();
-
-    address sender = _msgSender();
-
     uint256 rewardAmount = 1000;
-
-    bytes memory callDataTransfer = abi.encodeCall(IERC20.transfer, (sender, rewardAmount));
-
-    bytes memory callDataDelegateCall = abi.encodeCall(IBaseWorld(worldAddress).call, (systemId, callDataTransfer));
-
-    (bool success, ) = worldAddress.delegatecall(callDataDelegateCall);
-
-    require(success, "token transfer failed");
+    IERC20 token = IERC20(GameConfig.getTokenAddress());
+    token.transfer(_msgSender(), rewardAmount);
   }
 }
--- packages/contracts/test/systems/RewardSystem.t.sol
+++ packages/contracts/test/systems/RewardSystem.t.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.21;
 import "forge-std/console.sol";
-import { IWorld } from "../../src/codegen/world/IWorld.sol";
-import { MudTest } from "@latticexyz/world/test/MudTest.t.sol";
-import "../../src/codegen/index.sol";
-import "../../src/libraries/Libraries.sol";
+
+import { BaseTest } from "../BaseTest.sol";
 
 import { ResourceId } from "@latticexyz/store/src/ResourceId.sol";
 import { WorldResourceIdInstance } from "@latticexyz/world/src/WorldResourceId.sol";
@@ -12,23 +10,12 @@ import { WorldResourceIdInstance } from "@latticexyz/world/src/WorldResourceId.s
 import { _balancesTableId } from "@latticexyz/world-modules/src/modules/erc20-puppet/utils.sol";
 import { Balances } from "@latticexyz/world-modules/src/modules/tokens/tables/Balances.sol";
 import { Puppet } from "@latticexyz/world-modules/src/modules/puppet/Puppet.sol";
-import { IERC20Mintable } from "@latticexyz/world-modules/src/modules/erc20-puppet/IERC20Mintable.sol";
 
-contract RewardSystemTest is MudTest {
+import "../../src/codegen/index.sol";
+import "../../src/libraries/Libraries.sol";
+
+contract RewardSystemTest is BaseTest {
   using WorldResourceIdInstance for ResourceId;
-
-  IWorld world;
-  GameConfigData gameConfig;
-  address internal alice;
-  address internal bob;
-
-  function setUp() public override {
-    super.setUp();
-    world = IWorld(worldAddress);
-    gameConfig = GameConfig.get();
-    alice = address(111);
-    bob = address(222);
-  }
 
   function testReward() public {
     address token = gameConfig.tokenAddress;
